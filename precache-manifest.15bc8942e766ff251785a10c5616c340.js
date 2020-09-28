4af60e012fdc33fc97299a4cbb62bfd7bcc0cbc6
xtineroq <mcroque89@gmail.com>
2020-09-28 21:22:56 +1000

    Updates

---
+++ precache-manifest.15bc8942e766ff251785a10c5616c340.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db932d96b3d8444b1b81ad7bdbde4d16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed24258f9c3d6d1521d8",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "a65e252b75f5454ce1a0",
+    "url": "/static/css/main.0ed44172.chunk.css"
+  },
+  {
+    "revision": "ed24258f9c3d6d1521d8",
+    "url": "/static/js/2.72ecb9a9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.72ecb9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65e252b75f5454ce1a0",
+    "url": "/static/js/main.adef514b.chunk.js"
+  },
+  {
+    "revision": "97d533c4750052e7b0d4",
+    "url": "/static/js/runtime-main.ca19f78b.js"
+  },
+  {
+    "revision": "cf03f1a02521ee80f7a59b701f53597f",
+    "url": "/static/media/JS.cf03f1a0.png"
+  },
+  {
+    "revision": "dc084081e620400ba7711616d7c09ae6",
+    "url": "/static/media/arrow-down.dc084081.png"
+  },
+  {
+    "revision": "da53dc3418ca99ce8fec3846274d9cb8",
+    "url": "/static/media/bootstrap.da53dc34.png"
+  },
+  {
+    "revision": "b184b10520c89761d34c923a93138645",
+    "url": "/static/media/budget.b184b105.png"
+  },
+  {
+    "revision": "2c944e58fc31b783e651a4a0fe82805d",
+    "url": "/static/media/burger.2c944e58.png"
+  },
+  {
+    "revision": "6282ffde6ef31cd6bb9051fd0de21e50",
+    "url": "/static/media/c-logo.6282ffde.png"
+  },
+  {
+    "revision": "46874cc5908ef67a0c19ed4ba1e5a088",
+    "url": "/static/media/canva.46874cc5.png"
+  },
+  {
+    "revision": "d73a1ede4953ffb3a1dbf892f4297337",
+    "url": "/static/media/css3.d73a1ede.png"
+  },
+  {
+    "revision": "6e22a7632205333ae5363c605f375865",
+    "url": "/static/media/express.6e22a763.png"
+  },
+  {
+    "revision": "c069cffc29c9d47c6ab37445852a71fb",
+    "url": "/static/media/favicon.c069cffc.ico"
+  },
+  {
+    "revision": "856f31d9f475501c7552c97dbe727319",
+    "url": "/static/media/grey-pattern.856f31d9.jpg"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/static/media/html5.f2282f94.png"
+  },
+  {
+    "revision": "724c6cfa3a35fb9c922e8258a49cf907",
+    "url": "/static/media/invision.724c6cfa.png"
+  },
+  {
+    "revision": "639b02ee63c7918b47841bf74c278a8d",
+    "url": "/static/media/me.639b02ee.jpg"
+  },
+  {
+    "revision": "81ab19f015fdec36c23f040352db789a",
+    "url": "/static/media/mongodb.81ab19f0.png"
+  },
+  {
+    "revision": "3902d78dff49983277b0dad606b3d9b2",
+    "url": "/static/media/mysql.3902d78d.png"
+  },
+  {
+    "revision": "67ee91dce354f078bd142766f7256f00",
+    "url": "/static/media/nodejs.67ee91dc.png"
+  },
+  {
+    "revision": "302136430c1b46f7e373a50d34d5ad47",
+    "url": "/static/media/note.30213643.png"
+  },
+  {
+    "revision": "85d06aea76c4e82d6f72823597eb7180",
+    "url": "/static/media/pfp.85d06aea.png"
+  },
+  {
+    "revision": "85b020b8603363fce149bff9d31a0a7c",
+    "url": "/static/media/placeholder.85b020b8.jpg"
+  },
+  {
+    "revision": "0d8b9c6ece2870c4692ef62ad7f20f9f",
+    "url": "/static/media/planner.0d8b9c6e.png"
+  },
+  {
+    "revision": "03f72ea1bf89afdda376388da903c2c4",
+    "url": "/static/media/resume.03f72ea1.pdf"
+  },
+  {
+    "revision": "d3ee378715f1d4aa6fb79b24b68b5f39",
+    "url": "/static/media/sequelize.d3ee3787.png"
+  },
+  {
+    "revision": "b7b4b19552a34b390a09394ce1e0fa29",
+    "url": "/static/media/sky-bg.b7b4b195.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5e9ebfc2b45442f3bce3edf954b6d752",
+    "url": "/static/media/weather.5e9ebfc2.png"
+  }
+]);
